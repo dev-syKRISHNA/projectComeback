60f7b1184ccd086d05917a663f24709b2690991a
Sree Yadhu Krishna Menon <s.yadhukrishna.menon@outlook.com>
2026-06-18 01:39:59 +0530

    First Push

---
+++ JSfromScratch/js.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+num1 = 32.52374289832
+num2 = 21.99953453234239999
+sum = num1 + num2
+console.log("The sum of num1 and num2 is: ", Number(sum.toFixed(2)));
+console.log("Sum is",sum);
+console.log(typeof(sum));
